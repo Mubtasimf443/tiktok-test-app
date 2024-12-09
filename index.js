b4ec6a2200e33436531f637b01c63ed422803887
Mubtasim <mubtasimf443@gmail.com>
2024-12-09 20:13:01 +0600

    //callback 1

--- index.js
+++ index.js
@@ -12,10 +12,25 @@ import {fileURLToPath} from 'url'
 const app=express();
 const __dirname =path.dirname(fileURLToPath(import.meta.url));
 
+let 
+Tiktok_key='sbawlckvhm82juhm5x',
+tiktok_secret="GnAFzlruZRyLY6rgmu4L1UDLR0S85AN4",
+redirect_url='https://tiktok-video-upload-app.onrender.com/callback';
+
+const redirectionURLFunction = () => encodeURI(`https://www.tiktok.com/v2/auth/authorize?${Tiktok_key}=&response_type=code&scope=user.info.basic&redirect_uri=${redirect_url}&state=${Math.random().toString(36).substring(2)}`);
+
 
 app.get('/home', function(req,res) {
     res.sendFile( path.resolve(__dirname, './index.html'))
 })
+app.get('/get-code', (req,res) =>  res.redirect(redirectionURLFunction()))
+
+
+app.get('/callback',async function(req,res){
+    log(req.query);
+    res.sendStatus(200)
+})
+
 
 app.listen(3000, e => log("Thanks to Allah Subhanahu Oa Ta'ala, Server is working"));
 
