4c900fbb777ba612b2fa86a8c87305de41851218
Mubtasim <mubtasimf443@gmail.com>
2024-12-10 17:37:31 +0600

    redirectionURLFunction fixed

--- controllars/redirectionURLFunction.js
+++ controllars/redirectionURLFunction.js
@@ -3,14 +3,16 @@
 InshaAllah, By his marcy I will Gain Success 
 */
 
+import { TIKTOK_KEY, TIKTOK_REDIRECT_URI } from "./ENV.js"
+
 
 export  const redirectionURLFunction = () => {
     return (
         `https://www.tiktok.com/v2/auth/authorize?`
-        +`client_key=${Tiktok_key}`
+        +`client_key=${TIKTOK_KEY}`
         +`&scope=user.info.basic`
         +'&response_type=code'
-        +`&redirect_uri=${redirect_url}`
+        +`&redirect_uri=${TIKTOK_REDIRECT_URI}`
         +`&state=${Math.random().toString(36).substring(2)}`
     )
 }
